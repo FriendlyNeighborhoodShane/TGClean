3292bd141229c74835f703a2a0bec2d8dbcbc862
FriendlyNeighborhoodShane <39659394+FriendlyNeighborhoodShane@users.noreply.github.com>
2019-11-14 18:05:48 +0400

    add config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# Config file for tgclean
+
+# API id and hash obtained during setup
+tg_api_id = ""
+tg_api_hash = ""
+
+# Delete threshhold
+delete_before_days = 14
+
+# Array of chats to purge
+targets = [
+  "https://t.me/joinchat/abcdef",
+  "@abcdef",
+  -123456
+]  
