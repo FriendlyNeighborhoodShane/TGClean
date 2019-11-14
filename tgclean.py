4ab122466e64d9ca3d4a0d4e8e7892026e5edaee
FriendlyNeighborhoodShane <39659394+FriendlyNeighborhoodShane@users.noreply.github.com>
2019-11-14 18:57:52 +0400

    Update

--- tgclean.py
+++ tgclean.py
@@ -5,27 +5,18 @@
 # Source link for curious people
 sauce = "https://github.com/FriendlyNeighborhoodShane/TGClean"
 
-# Delete threshhold
-delete_before_days = 14
-
-# Array of chats to purge
-targets = [
-  "https://t.me/joinchat/abcdef",
-  "@abcdef",
-  -123456
-]  
-
 import time
 from pyrogram import Client
+import config.py
 
 print("TGClean")
 print("-- Starting up delete routine...")
 
-# Create client object with name
-app = Client("TGClean")
-
 delete_before = int(time.time() - (delete_before_days * 24 * 60 * 60))
 print("-- Deleting messages from before " + str(delete_before))
+
+# Create client object with name
+app = Client("TGClean", api_id = tg_api_id, api_hash = tg_api_hash)
 
 app.start()
 print("-- Client started")
@@ -44,12 +35,11 @@ for target in targets:
   messages = []
   messagecount = 0
   deleted = 0
-  undeleted = 0
 
   # Ask for array of messages (oldest to newest) and iterate through it
   for message in app.iter_history(id, reverse = True):
 
-    # If limit is reached, delete messages and reset array
+    # TG only allows deleting upto 100 messages at once, then delete messages and reset array
     if(messagecount == 100):
       print("    ++ Deleting " + str(messagecount) + " messages at " + str(message.date))
       app.delete_messages(id, messages)
@@ -63,7 +53,7 @@ for target in targets:
       messagecount = messagecount + 1
     else:
       print("    ++ Found a too new message sent at " + str(message.date))
-      undeleted = undeleted + 1
+      break
 
   # Delete leftover messages
   if(messagecount > 0):
@@ -72,8 +62,7 @@ for target in targets:
     deleted = deleted + messagecount
 
   # Send result
-  app.send_message(id, "- - - TGCLEAN REPORT - - -\n\nFinished group purge at: " + str(time.time()) + "\n\nUserbot sauce at: " + sauce, disable_web_page_preview = True)
-  app.send_message(id, "Messages deleted: " + str(deleted) + "\nMessages not deleted: " + str(undeleted))
+  app.send_message(id, "- - - TGCLEAN REPORT - - -\n\nFinished group purge at: " + str(time.time()) + "\n\nMessages deleted: " + str(deleted) + "\n\nUserbot sauce at: " + sauce, disable_web_page_preview = True)
 
   print("-- Done")
 
