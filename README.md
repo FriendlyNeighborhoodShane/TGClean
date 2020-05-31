14d80552018928435b270cf4f3172cc3dd6a6667
FriendlyNeighborhoodShane <shane.880088.supw@gmail.com>
2020-05-31 12:32:23 +0530

    Add tgpurge script for cleaning only your own messages

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ A Telegram cleaning python script
 Based on work by theel0ja
 Modification by FriendlyNeighborhoodShane
 
-A userbot script that deletes all messages in a group older than a specified time.
+TGClean is a userbot script that deletes all messages in a group older than a specified time as an admin.
+
+TGPurge is a userbot script that deletes all of your messages in a group older than a specified time (does not need admin).
 
 ## Installation
 
@@ -18,20 +20,24 @@ A userbot script that deletes all messages in a group older than a specified tim
 
 ## Configuration
 
+The same configuration file is shared between both the scripts.
+
 Open config.py.
 
 Obtain api_id and api_hash as described [here](https://docs.pyrogram.org/). But instead of putting it in config.ini, put them as tg_api_id and tg_api_hash in config.py (in the quotes).
 
-In the targets array, add a ChatGroup object for every chat/days combo you need. The first argument is the list of chats, and the second is the last N days you want to keep messages for. There can be as many ChatGroup objects as you want (long as your computer doesnlt run out of memory :P).
+In the targets array, add a ChatGroup object for every chat/days combo you need. The first argument is the list of chats, and the second is the last N days you want to keep messages for. There can be as many ChatGroup objects as you want (long as your computer doesn't run out of memory :P).
 
 ## Usage
 
 ```
-
 > python ./tgclean.py
-
+```
+or
+```
+> python ./tgpurge.py
 ```
 
-The first time you run it, it will ask you to login with your phone number and verification code. After that, your sessions will be stored in the TGClean.session file.
+The first time you run it, it will ask you to login with your phone number and verification code. After that, your sessions will be stored in the TGClean.session file (shared between both scripts).
 
 Alternatively, use login.py to login and create the session without actually cleaning anything.
---
+++ tgpurge.py
@@ -0,0 +1,94 @@
+# TGPurge script
+# By theel0ja
+# Modification by FriendlyNeighborhoodShane
+
+import time;
+
+from pyrogram import Client;
+from pyrogram.errors import InviteHashExpired, PeerIdInvalid, UsernameInvalid, UserNotParticipant;
+
+import config;
+
+print(" ");
+print("TGPurge Cleaning Script");
+
+# Create client object with name
+print(" ");
+print(" - Starting client");
+app = Client("TGClean", api_id = config.tg_api_id, api_hash = config.tg_api_hash);
+app.start();
+
+print(" ");
+print(" - Starting delete routine");
+
+selfid = app.get_me().id;
+
+for target in config.targets:
+
+  print(" ");
+  print(" - Deleting messages older than " + str(target.delete_before_days) + " days");
+  delete_before = int(time.time() - (target.delete_before_days * 24 * 60 * 60));
+
+  for chat in target.chatlist:
+
+    # Get target's chat ID and title
+    try:
+      chatobj = app.get_chat(chat);
+    except(PeerIdInvalid, UsernameInvalid, ValueError):
+      print(" ");
+      print(" ! Given value leads to invalid chat " + str(chat));
+      continue;
+    except(InviteHashExpired):
+      print(" ");
+      print(" ! Given chat invite has expired " + str(chat));
+      continue;
+
+    print(" ");
+    print(" - Deleting from chat " + str(chat));
+    print("   -- Chat ID: " + str(chatobj.id));
+    print("   -- Chat name: " + chatobj.title);
+
+    # Check that we have delete perms
+    try:
+      memberobj = app.get_chat_member(chatobj.id, "self");
+    except(UserNotParticipant):
+      print(" ")
+      print("   !! The user is not a participant in this chat");
+      continue;
+
+    # Create empty array and variables
+    messages = [];
+    messagecount = 0;
+    deleted = 0;
+
+    # Ask for array of messages (oldest to newest) and iterate through it
+    for message in app.iter_history(chatobj.id, reverse = True):
+
+      # TG only allows deleting upto 100 messages at once, then delete messages and reset array
+      if(messagecount == 100):
+        print("     ++ Deleting " + str(messagecount) + " messages at " + str(message.date));
+        app.delete_messages(chatobj.id, messages);
+        deleted = deleted + messagecount;
+        messages = [];
+        messagecount = 0;
+
+      # If message is old, add its id to array
+      if(delete_before > message.date):
+        if(message.from_user.id == selfid):
+          messages += [message.message_id];
+          messagecount = messagecount + 1;
+      else:
+        print("     ++ Found a too new message sent at " + str(message.date));
+        break;
+
+    # Delete leftover messages
+    if(messagecount > 0):
+      print("     ++ Deleting "  + str(messagecount) + " messages at end");
+      app.delete_messages(chatobj.id, messages);
+      deleted = deleted + messagecount;
+
+    print("   -- Done");
+
+print(" ");
+print(" - Stopping Client");
+app.stop();
